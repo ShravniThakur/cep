4e82eb4c786262d7390dccc19288857a3080bb2f
ShravniThakur <pictshravni@gmail.com>
2026-04-05 17:02:58 +0530

    vite config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8934',
+        target: 'https://cep-z7ow.onrender.com',
         changeOrigin: true,
       },
     },
